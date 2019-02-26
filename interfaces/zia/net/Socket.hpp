6743375e6af01c2571b7173f1c5b5ffa68d670e6
Victor CUTILLAS <victor.cutillas@epitech.eu>
2019-02-26 15:48:04 +0100

    zia::net::Socket is cross-platform

--- interfaces/zia/net/Socket.hpp
+++ interfaces/zia/net/Socket.hpp
@@ -1,12 +1,36 @@
+//
+// EPITECH PROJECT, 2019
+// ZiAPI
+// File description:
+// Socket.hpp
+//
+
 #pragma once
+
+#include "zia/sys/Config.hpp"
+
+#ifdef ZIA_SYS_WINDOWS
+#    include <winsock2.h>
+#endif
 
 namespace zia {
 namespace net {
+
+#ifdef ZIA_SYS_WINDOWS
+
+///
+/// \brief Socket type
+///
+using Socket = SOCKET;
+
+#else
 
 ///
 /// \brief Socket type
 ///
 using Socket = int;
+
+#endif
 
 }  // namespace net
 }  // namespace zia
