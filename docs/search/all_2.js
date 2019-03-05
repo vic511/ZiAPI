f5464296a4223bdcea818c3089cdc3abe8a32d99
Victor CUTILLAS <victor.cutillas@epitech.eu>
2019-03-05 22:10:57 +0100

    Doxygen update

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['defaultvalue',['defaultValue',['../structzia_1_1api_1_1ConfigKey.html#af0f0cd68ccbcb911eede934a3958a087',1,'zia::api::ConfigKey']]],
   ['doread',['doRead',['../classzia_1_1api_1_1INetworkModule.html#ac28ddc39f2f273bbfe56651bc2fdd2bd',1,'zia::api::INetworkModule']]],
-  ['dowrite',['doWrite',['../classzia_1_1api_1_1INetworkModule.html#a305101fba4712da08df9ad1b93309a48',1,'zia::api::INetworkModule']]]
+  ['dowrite',['doWrite',['../classzia_1_1api_1_1INetworkModule.html#a2eb272aa7226633e286e359885497ac7',1,'zia::api::INetworkModule']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['doread',['doRead',['../classzia_1_1api_1_1INetworkModule.html#ac28ddc39f2f273bbfe56651bc2fdd2bd',1,'zia::api::INetworkModule']]],
-  ['dowrite',['doWrite',['../classzia_1_1api_1_1INetworkModule.html#a305101fba4712da08df9ad1b93309a48',1,'zia::api::INetworkModule']]]
+  ['dowrite',['doWrite',['../classzia_1_1api_1_1INetworkModule.html#a2eb272aa7226633e286e359885497ac7',1,'zia::api::INetworkModule']]]
 ];
